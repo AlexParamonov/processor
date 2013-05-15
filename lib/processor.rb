9a50a028b051c50dc538b7cffb8be27cfee0349e
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2013-05-15 21:09:15 +0300

    Preload all files on startup

--- lib/processor.rb
+++ lib/processor.rb
@@ -1,4 +1,20 @@
 require "processor/version"
 
+require "processor/data/array_processor"
+require "processor/data/batch_processor"
+require "processor/data/null_processor"
+require "processor/data/solr_processor"
+require "processor/data/solr_pages_processor"
+require "processor/data/csv_processor"
+
+require "processor/observer/logger"
+require "processor/observer/null_observer"
+
+require "processor/process_runner/successive"
+require "processor/process_runner/threads"
+
+require "processor/runner"
+require "processor/thread"
+
 module Processor
 end
