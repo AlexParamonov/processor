5c53d674fa001a17f9fccd0a707c68b93dec183b
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2013-05-26 15:50:39 +0000

    minor spec edits

--- spec/processor/data/array_processor_spec.rb
+++ spec/processor/data/array_processor_spec.rb
@@ -3,8 +3,7 @@ require 'processor/data/array_processor'
 
 describe Processor::Data::ArrayProcessor do
   it "should have total records count equals to count of records" do
-    records = *1..5
-    subject.stub(records: records)
+    subject.stub(records: 1..5)
     subject.total_records.should eq 5
   end
 end
