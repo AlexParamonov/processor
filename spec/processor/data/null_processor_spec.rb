b15503960f42e7b00936c8f82e47446b6978ea94
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2013-06-05 19:19:16 +0300

    Use real data in null_processor_spec

--- spec/processor/data/null_processor_spec.rb
+++ spec/processor/data/null_processor_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper_lite'
 require 'processor/data/null_processor'
+require 'processor/runner'
+require 'processor/process_runner/successive'
 
 describe Processor::Data::NullProcessor do
   it "should have zero records" do
@@ -7,8 +9,26 @@ describe Processor::Data::NullProcessor do
     subject.total_records.should be_zero
   end
 
-  %w[start finish finalize error process records total_records].each do |method|
-    it { subject.should respond_to method }
+  describe "required methods" do
+    it "should respond to all happy path methods" do
+      collector = Class.new do
+        def records; [1] end
+        def method_missing(method, *)
+          @methods = @methods.to_a << method
+        end
+        attr_reader :methods
+      end.new
+
+      runner = Processor::Runner.new collector
+      runner.run Processor::ProcessRunner::Successive.new
+      collector.methods.each do |method|
+        subject.should respond_to method
+      end
+    end
+
+    %w[record_error error total_records].each do |method|
+      it { subject.should respond_to method }
+    end
   end
 end
 
