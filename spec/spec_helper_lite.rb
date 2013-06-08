045a81eea9abddc786488086c47f15cb438acd85
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2013-06-08 12:30:10 +0300

    Add Coveralls

--- spec/spec_helper_lite.rb
+++ spec/spec_helper_lite.rb
@@ -1,6 +1,11 @@
-require 'pry' unless Processor::RUNNING_ON_CI
+if Processor::RUNNING_ON_CI
+  require 'coveralls'
+  Coveralls.wear!
+else
+  require 'pry'
+end
 
 ENV['RAILS_ENV'] ||= 'test'
-require "rspec"
+require 'rspec'
 
 $: << File.expand_path('../lib', File.dirname(__FILE__))
